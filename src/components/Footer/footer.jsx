2f2505c2bf05dc5147c5ddc9eb9b1383f5908671
paulohenrique <paulohenriquedeoliveira2006@gmail.com>
2024-06-13 07:53:48 -0300

    Add files via upload

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,11 @@
+import styles from './footer.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito com ❣️ por Sandra Alves © 2023
+        </footer>
+    )
+}
+
+export default Footer
