11b381f4e6a67304665156fec2d408a5298f4599
paulohenrique <paulohenriquedeoliveira2006@gmail.com>
2024-06-13 07:59:54 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import { useState } from 'react'
-
-import './App.css'
-import Header from './components/Header/header'
-import Footer from './components/Footer/footer'
+import AppRoutes from './routes'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-     <Header/>
-      <section className='container'>
-        <div className='apresentacao'>
-          <p>
-            Olá, sou <br/>
-            <span>Sandra Alves</span> <br/>
-            Dev Full Stack
-          </p>
-          <button className='btn btn-red'>
-            Saiba mais sobre mim
-          </button>
-        </div>
-        <figure>
-          <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
-        </figure>
-      </section>
-     <Footer/>
-    </>
+    <AppRoutes />
   )
 }
 
