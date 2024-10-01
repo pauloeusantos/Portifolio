cca8e9e74b34add195c8c458a458c18f409ae1da
pauloeusantos <paulohenriquedeoliveira2006@gmail.com>
2024-09-30 21:20:52 -0300

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,29 @@
-import React from 'react';
+
 
 const Header = ({ onOpen }) => {
-
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
-
   return (
-    <header id="hero" className="bg-gray-800 fixed top-0 w-full z-50 py-4 shadow-lg">
+    <header className="fixed top-0 w-full z-50 py-4 shadow-lg bg-gray-800 backdrop-blur-lg bg-opacity-70 transition-all duration-300">
       <nav className="text-center">
         <ul className="flex justify-center space-x-10 list-none">
           <li>
-            <a 
-              href="#hero" 
-              onClick={scrollToTop} 
+            <a
+              href="#hero"
               className="text-white text-lg no-underline hover:text-blue-300 transition-colors duration-300"
             >
               Home
             </a>
           </li>
           <li>
-            <a 
-              href="#about" 
+            <a
+              href="#about"
               className="text-white text-lg no-underline hover:text-blue-300 transition-colors duration-300"
             >
               About
             </a>
           </li>
           <li>
-            <button 
-              onClick={onOpen} 
+            <button
+              onClick={onOpen}
               className="text-white text-lg no-underline hover:text-blue-300 transition-colors duration-300"
             >
               Projects
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,48 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from '../components/Header';
 import Container from '../components/Container';
 import Footer from '../components/Footer';
 import DragCloseDrawer from '../components/DragCloseDrawer';
 import About from '../components/About';
+import { ExternalLink, Github } from 'lucide-react'; 
+
 
 const projects = [
-  { id: 1, title: 'Movie List', description: 'Os dados dos filmes são obtidos a partir de uma API externa, proporcionando informações atualizadas sobre títulos, sinopses, classificações e outros detalhes relevantes.', link: 'https://santfilmes.netlify.app/' },
-  { id: 2, title: 'Api Movies', description: 'React.js consumindo API', link: 'https://api-movies-virid.vercel.app/.com' },
-  { id: 3, title: 'RoomApp', description: 'Utilizando a biblioteca Room para gerenciamento de banco de dados local. Este aplicativo permite a criação, leitura, atualização e exclusão (CRUD) de dados armazenados localmente no dispositivo.', link: 'https://github.com/pauloeusantos/RoomApp' },
-  { id: 4, title: 'RoomApp-Inventory', description: 'Este aplicativo permite aos usuários adicionar itens ao inventário, especificar o preço e a quantidade em estoque, e gerenciar esses itens de forma eficiente.', link: 'https://github.com/pauloeusantos/RoomApp-Inventory' },
+  {
+    id: 1,
+    title: 'Movie List',
+    description: 'Os dados dos filmes são obtidos a partir de uma API externa, proporcionando informações atualizadas sobre títulos, sinopses, classificações e outros detalhes relevantes.',
+    demoLink: 'https://santfilmes.netlify.app/',
+    codeLink: 'https://github.com/pauloeusantos/RoomApp' 
+  },
+  {
+    id: 2,
+    title: 'Api Movies',
+    description: 'React.js consumindo API',
+    demoLink: 'https://api-movies-virid.vercel.app/',
+    codeLink: 'https://github.com/pauloeusantos/ApiMovies' 
+  },
+  {
+    id: 3,
+    title: 'RoomApp',
+    description: 'Utilizando a biblioteca Room para gerenciamento de banco de dados local.',
+    demoLink: 'https://github.com/pauloeusantos/RoomApp',
+    codeLink: 'https://github.com/pauloeusantos/RoomApp'
+  },
+  {
+    id: 4,
+    title: 'RoomApp-Inventory',
+    description: 'Este aplicativo permite aos usuários adicionar itens ao inventário, especificar o preço e a quantidade em estoque, e gerenciar esses itens de forma eficiente.',
+    demoLink: 'https://github.com/pauloeusantos/RoomApp-Inventory',
+    codeLink: 'https://github.com/pauloeusantos/RoomApp-Inventory'
+  },
 ];
 
 const Home = () => {
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  
 
   return (
     <>
@@ -25,30 +51,35 @@ const Home = () => {
       <About />
       <Footer />
 
-
       <DragCloseDrawer open={open} setOpen={setOpen}>
         <div className="mx-auto max-w-2xl space-y-4 text-neutral-400">
           <h2 className="text-4xl font-bold text-neutral-200">Meus Projetos</h2>
           <div className="flex flex-col space-y-4">
-            {projects.map(project => (
+         
+            {projects.map((project) => (
+  
               <div key={project.id} className="bg-neutral-800 p-4 rounded-lg shadow-lg transition-shadow duration-300 hover:shadow-xl">
-                <h3 className="text-lg font-bold">
-                  <a
-                    href={project.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-white hover:text-blue-500 transition-colors duration-300"
-                  >
-                    {project.title}
-                  </a>
-                </h3>
+                <h3 className="text-lg font-bold text-white">{project.title}</h3>
                 <p className="text-neutral-300">{project.description}</p>
+                <div className="flex justify-between mt-4">
+                  <a href={project.demoLink} target="_blank" rel="noopener noreferrer">
+                    <button className="text-sm text-blue-500 border border-blue-500 px-4 py-2 rounded-lg flex items-center space-x-2 hover:bg-blue-500 hover:text-white transition-colors">
+                      <ExternalLink className="w-4 h-4" />
+                      <span>Demo</span>
+                    </button>
+                  </a>
+                  <a href={project.codeLink} target="_blank" rel="noopener noreferrer">
+                    <button className="text-sm text-gray-500 border border-gray-500 px-4 py-2 rounded-lg flex items-center space-x-2 hover:bg-gray-500 hover:text-white transition-colors">
+                      <Github className="w-4 h-4" />
+                      <span>Code</span>
+                    </button>
+                  </a>
+                </div>
               </div>
             ))}
           </div>
         </div>
       </DragCloseDrawer>
-
     </>
   );
 }
